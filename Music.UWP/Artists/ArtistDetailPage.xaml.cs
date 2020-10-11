20c2168224b7ca8d9b4e1a351b67381ab491d974
Edwin Hattink <edwin@hatt.ink>
2020-10-11 20:52:38 +0200

    refactor: deleted navigation code

--- Music.UWP/Artists/ArtistDetailPage.xaml.cs
+++ Music.UWP/Artists/ArtistDetailPage.xaml.cs
@@ -12,18 +12,9 @@ namespace Music.UWP.Artists
 {
     public sealed partial class ArtistDetailPage : Page
     {
-        private static DependencyProperty s_itemProperty
-            = DependencyProperty.Register("Artist", typeof(Artist), typeof(ArtistDetailPage), new PropertyMetadata(null));
-
-        public static DependencyProperty ItemProperty
-        {
-            get { return s_itemProperty; }
-        }
-
         public Artist Artist
         {
-            get { return (Artist)GetValue(s_itemProperty); }
-            set { SetValue(s_itemProperty, value); }
+            get; set;
         }
 
         public ArtistDetailPage()
@@ -37,44 +28,7 @@ namespace Music.UWP.Artists
 
             // Parameter is item ID
             Artist = ArtistRepository.GetById((int)e.Parameter);
-
-            var backStack = Frame.BackStack;
-            var backStackCount = backStack.Count;
-
-            if (backStackCount > 0)
-            {
-                var masterPageEntry = backStack[backStackCount - 1];
-                backStack.RemoveAt(backStackCount - 1);
-
-                // Doctor the navigation parameter for the master page so it
-                // will show the correct item in the side-by-side view.
-                var modifiedEntry = new PageStackEntry(
-                    masterPageEntry.SourcePageType,
-                    Artist.Id,
-                    masterPageEntry.NavigationTransitionInfo
-                    );
-                backStack.Add(modifiedEntry);
-            }
-
-            // Register for hardware and software back request from the system
-            SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
-            systemNavigationManager.BackRequested += DetailPage_BackRequested;
-            systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
         }
 
-        private void OnBackRequested()
-        {
-            // Page above us will be our master view.
-            // Make sure we are using the "drill out" animation in this transition.
-
-            Frame.GoBack(new DrillInNavigationTransitionInfo());
-        }
-
-        private void DetailPage_BackRequested(object sender, BackRequestedEventArgs e)
-        {
-            e.Handled = true;
-
-            OnBackRequested();
-        }
     }
 }
--- Music.UWP/Genres/GenreDetailPage.xaml.cs
+++ Music.UWP/Genres/GenreDetailPage.xaml.cs
@@ -23,18 +23,9 @@ namespace Music.UWP.Genres
 {
     public sealed partial class GenreDetailPage : Page
     {
-        private static DependencyProperty s_itemProperty
-            = DependencyProperty.Register("Genre", typeof(Genre), typeof(GenreDetailPage), new PropertyMetadata(null));
-
-        public static DependencyProperty ItemProperty
-        {
-            get { return s_itemProperty; }
-        }
-
         public Genre Genre
         {
-            get { return (Genre)GetValue(s_itemProperty); }
-            set { SetValue(s_itemProperty, value); }
+            get; set;
         }
 
         public GenreDetailPage()
@@ -48,44 +39,7 @@ namespace Music.UWP.Genres
 
             // Parameter is item ID
             Genre = GenreRepository.GetById((int)e.Parameter);
-
-            var backStack = Frame.BackStack;
-            var backStackCount = backStack.Count;
-
-            if (backStackCount > 0)
-            {
-                var masterPageEntry = backStack[backStackCount - 1];
-                backStack.RemoveAt(backStackCount - 1);
-
-                // Doctor the navigation parameter for the master page so it
-                // will show the correct item in the side-by-side view.
-                var modifiedEntry = new PageStackEntry(
-                    masterPageEntry.SourcePageType,
-                    Genre.Id,
-                    masterPageEntry.NavigationTransitionInfo
-                    );
-                backStack.Add(modifiedEntry);
-            }
-
-            // Register for hardware and software back request from the system
-            SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
-            systemNavigationManager.BackRequested += DetailPage_BackRequested;
-            systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
-        }
-
-        private void OnBackRequested()
-        {
-            // Page above us will be our master view.
-            // Make sure we are using the "drill out" animation in this transition.
-
-            Frame.GoBack(new DrillInNavigationTransitionInfo());
-        }
-
-        private void DetailPage_BackRequested(object sender, BackRequestedEventArgs e)
-        {
-            e.Handled = true;
-
-            OnBackRequested();
+         
         }
     }
 }
