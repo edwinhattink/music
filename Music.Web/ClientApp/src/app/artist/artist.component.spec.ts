5bb101cae81f51460c806ed9bd3fdd78be98cc84
Edwin Hattink <edwin@hatt.ink>
2021-08-24 09:59:52 +0200

    angular 11 update

--- Music.Web/ClientApp/src/app/artist/artist.component.spec.ts
+++ Music.Web/ClientApp/src/app/artist/artist.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ArtistComponent } from './artist.component';
 
@@ -6,7 +6,7 @@ describe('ArtistComponent', () => {
   let component: ArtistComponent;
   let fixture: ComponentFixture<ArtistComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ArtistComponent ]
     })
