90658c92b1ac5fe1e9917b56e865875b19d2e50b
Edwin Hattink <44589126+edwinhattink@users.noreply.github.com>
2023-03-22 11:44:55 +0100

    scan controller voortgang

--- Music.Web/Controllers/ScanController.cs
+++ Music.Web/Controllers/ScanController.cs
@@ -21,7 +21,7 @@ namespace Music.Web.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<object>> Scan()
+        public async Task<ActionResult<IEnumerable<object>>> Scan()
         {
             var mp3s = Directory.GetFiles("C:\\git\\Music\\Music.Web\\wwwroot\\Music", "*.mp3", SearchOption.AllDirectories);
 
@@ -29,156 +29,150 @@ namespace Music.Web.Controllers
             {
                 var tfile = TagLib.File.Create(mp3Path);
 
-                //TODO: async maken
-                var genre = GetOrCreateGenre(tfile.Tag.JoinedGenres);
-                var album = GetOrCreateAlbum(tfile.Tag.Album, checked((int)tfile.Tag.Year));
-                var disc = GetOrCreateDisc(album.Id, checked((int)tfile.Tag.Disc));
-                var track = GetOrCreateTrack(mp3Path, tfile.Tag.Title, checked((int) tfile.Tag.Track), genre.Id, disc.Id);
-                var artist = GetOrCreateArtist(tfile.Tag.JoinedPerformers);
-                GetOrCreateContribution(artist.Id, track.Id);
-                var albumArtist = GetOrCreateArtist(tfile.Tag.JoinedAlbumArtists);
-                
-                
+                var genre = await GetOrCreateGenre(tfile.Tag.JoinedGenres);
+                var album = await GetOrCreateAlbum(tfile.Tag.Album, checked((int)tfile.Tag.Year));
+                var artist = await GetOrCreateArtist(tfile.Tag.JoinedPerformers);
+                var albumArtist = await GetOrCreateArtist(tfile.Tag.JoinedAlbumArtists);
 
+                var disc = await GetOrCreateDisc(album.Id, checked((int)tfile.Tag.Disc));
+
+                var track = await GetOrCreateTrack(mp3Path, tfile.Tag.Title, checked((int) tfile.Tag.Track), genre.Id, disc.Id);
+
+                await GetOrCreateDiscContribution(albumArtist.Id, disc.Id);
+                await GetOrCreateContribution(artist.Id, track.Id);              
             }
 
             return mp3s;
         }
 
-        private Contribution GetOrCreateContribution(int artistId, int trackId)
+        private async Task<Contribution> GetOrCreateContribution(int artistId, int trackId)
         {
-            //TODO: betere performance van maken door NIET eerst alles op te halen (IQueryable<Contribution>)
-            // IQueryable<Contribution> query = _context.Contributions;
-            IEnumerable<Contribution> query = _context.Contributions.ToList();
-            query = query.Where(c => c.ArtistId == artistId && c.TrackId == trackId);
-            var contribution = query.FirstOrDefault(new Contribution() { 
-                ArtistId = artistId,
-                TrackId = trackId,
-                ContributionType = ContributionType.Main
-            });
+            var contribution = await _context.Contributions
+                .Where(c => c.ArtistId == artistId && c.TrackId == trackId)
+                .SingleOrDefaultAsync();
 
-            if (contribution.Id < 1)
+            if (contribution == null)
             {
+                contribution = new Contribution()
+                {
+                    ArtistId = artistId,
+                    TrackId = trackId,
+                    ContributionType = ContributionType.Main
+                };
                 _context.Contributions.Add(contribution);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
             }
 
             return contribution;
         }
 
-        private DiscContribution GetOrCreateDscContribution(int artistId, int discId)
+        private async Task<DiscContribution> GetOrCreateDiscContribution(int artistId, int discId)
         {
-            //TODO: betere performance van maken door NIET eerst alles op te halen (IQueryable<DiscContribution>)
-            // IQueryable<DiscContribution> query = _context.DiscContributions;
-            IEnumerable<DiscContribution> query = _context.DiscContributions.ToList();
-            query = query.Where(c => c.ArtistId == artistId && c.DiscId == discId);
-            var contribution = query.FirstOrDefault(new DiscContribution()
-            {
-                ArtistId = artistId,
-                DiscId = discId
-            });
+            var contribution = await _context.DiscContributions
+                .Where(c => c.ArtistId == artistId && c.DiscId == discId)
+                .SingleOrDefaultAsync();
 
-            if (contribution.Id < 1)
+            if (contribution == null)
             {
+                contribution = new DiscContribution()
+                {
+                    ArtistId = artistId,
+                    DiscId = discId
+                };
                 _context.DiscContributions.Add(contribution);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
             }
 
             return contribution;
         }
 
-        private Genre GetOrCreateGenre(string genreName)
+        private async Task<Genre> GetOrCreateGenre(string genreName)
         {
-            //TODO: betere performance van maken door NIET eerst alles op te halen (IQueryable<Genre>)
-            IEnumerable<Genre> query = _context.Genres.ToList();
-            query = query.Where(g => g.Name == genreName);
-            var genre = query.FirstOrDefault(new Genre() { Name = genreName });
+            var genre = await _context.Genres
+                .Where(g => g.Name == genreName)
+                .SingleOrDefaultAsync();
 
-            //TODO: betere check?
-            if (genre.Id < 1)
+            if (genre == null)
             {
+                genre = new Genre() { Name = genreName };
+
                 _context.Genres.Add(genre);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
             }
 
             return genre;
         }
 
-        private Track GetOrCreateTrack(string fileName, string title, int number, int genreId, int discId)
+        private async Task<Track> GetOrCreateTrack(string fileName, string title, int number, int genreId, int discId)
         {
-            //TODO: betere performance van maken door NIET eerst alles op te halen (IQueryable<Track>)
-            IEnumerable<Track> query = _context.Tracks.ToList();
-            query = query.Where(t => t.FileName == fileName);
-            var track = query.FirstOrDefault(new Track()
-            {
-                FileName = fileName,
-                Name = title,
-                Number = number,
-                GenreId = genreId,
-                DiscId = discId
-            });
+            var track = await _context.Tracks
+                .Where(t => t.FileName == fileName)
+                .SingleOrDefaultAsync();
 
-            //TODO: betere check?
-            if (track.Id < 1)
+            if (track == null)
             {
+                track = new Track()
+                {
+                    FileName = fileName,
+                    Name = title,
+                    Number = number,
+                    GenreId = genreId,
+                    DiscId = discId
+                };
                 _context.Tracks.Add(track);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
             }
 
             return track;
         }
 
-        private Artist GetOrCreateArtist(string artistName)
+        private async Task<Artist> GetOrCreateArtist(string artistName)
         {
-            //TODO: betere performance van maken door NIET eerst alles op te halen (IQueryable<Artist>)
-            IEnumerable<Artist> query = _context.Artists.ToList();
-            query = query.Where(a => a.Name == artistName);
-            var artist = query.FirstOrDefault(new Artist() { Name = artistName });
+            var artist = await _context.Artists
+                .Where(a => a.Name == artistName)
+                .SingleOrDefaultAsync();
 
-            //TODO: betere check?
-            if (artist.Id < 1)
+            if (artist == null)
             {
+                artist = new Artist() { Name = artistName };
                 _context.Artists.Add(artist);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
             }
 
             return artist;
         }
 
-        //TODO: disc?
-        private Album GetOrCreateAlbum(string albumName, int year)
+        private async Task<Album> GetOrCreateAlbum(string albumName, int year)
         {
-            //TODO: betere performance van maken door NIET eerst alles op te halen (IQueryable<Album>)
-            IEnumerable<Album> query = _context.Albums.ToList();
-            query = query.Where(a => a.Name == albumName);
-            var album = query.FirstOrDefault(new Album() { Name = albumName, ReleaseYear = year });
+            var album = await _context.Albums
+                .Where(a => a.Name == albumName)
+                .SingleOrDefaultAsync();
 
-            //TODO: betere check?
-            if (album.Id < 1)
+            if (album == null)
             {
+                album = new Album() { Name = albumName, ReleaseYear = year };
                 _context.Albums.Add(album);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
             }
 
             return album;
         }
-        
-        private Disc GetOrCreateDisc(int albumId, int number)
-        {
-            //TODO: betere performance van maken door NIET eerst alles op te halen (IQueryable<Disc>)
-            IEnumerable<Disc> query = _context.Discs.ToList();
-            query = query.Where(d => d.AlbumId == albumId && d.Number == number);
-            var disc = query.FirstOrDefault(new Disc()
-            {
-                AlbumId = albumId,
-                Number = number,
-            });
 
-            //TODO: betere check?
-            if (disc.Id < 1)
+        private async Task<Disc> GetOrCreateDisc(int albumId, int number)
+        {
+            var disc = await _context.Discs
+                .Where(d => d.AlbumId == albumId && d.Number == number)
+                .SingleOrDefaultAsync();
+
+            if (disc == null)
             {
+                disc = new Disc()
+                {
+                    AlbumId = albumId,
+                    Number = number,
+                };
                 _context.Discs.Add(disc);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
             }
 
             return disc;
